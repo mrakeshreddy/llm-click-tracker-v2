2941eb9bbe6a46f2101f2a243b1454602f8285cc
mrakeshreddy <61080890+mrakeshreddy@users.noreply.github.com>
2025-07-15 20:42:56 +0530

    Add files via upload

---
+++ utils/oauthConfig.js
@@ -0,0 +1,5 @@
+export const OAUTH_CONFIG = {
+  client_id: "764668315789-efimoblb025il5bab5kfud2s8lua9k0d.apps.googleusercontent.com",
+  client_secret: "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",
+  redirect_uri: "http://localhost:3000/api/gsc-auth-callback"
+};
